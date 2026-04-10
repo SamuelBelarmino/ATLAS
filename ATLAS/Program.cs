8452e2414b0fe76e02d269f75c9de3f09bf79580
SamuelBelarmino <samuel.bsilva@sptech.school>
2026-04-10 00:35:21 -0300

    adciona referência a Application/Infrastructure e config inicial

--- ATLAS/Program.cs
+++ ATLAS/Program.cs
@@ -1,15 +1,19 @@
+using Invest.API.Middleware;
+using Invest.Application;
+using Invest.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +21,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
